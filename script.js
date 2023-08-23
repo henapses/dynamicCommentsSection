350d4608c7d76c593135fec947603d4e5524e3ed
henapses <stephane.alleman@gmail.com>
2023-08-23 18:10:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function createEnclosingDiv() {
+  const enclosingDiv = document.createElement("div");
+  enclosingDiv.classList = "comment";
+  return enclosingDiv;
+}
+
+function createAvatar(initials) {
+  let newAvatar = document.createElement("span");
+  newAvatar.innerText = initials;
+  newAvatar.classList = "avatar";
+  return newAvatar;
+}
+
+function createText(text) {
+  let newText = document.createElement("span");
+  newText.classList.add("text");
+  newText.innerText = text;
+  return newText;
+}
+
+function resetFields() {
+  document.getElementById("initials").value = "";
+  document.getElementById("commentText").value = "";
+}
+
+function addComment() {
+  let commentsDiv = document.getElementById("commentSection");
+  let initialsInput = document.getElementById("initials").value;
+  let textInput = document.getElementById("commentText").value;
+  let enclosingDiv = createEnclosingDiv();
+  let avatar = createAvatar(initialsInput);
+  let text = createText(textInput);
+  enclosingDiv.appendChild(avatar);
+  enclosingDiv.appendChild(text);
+  commentsDiv.appendChild(enclosingDiv);
+  resetFields();
+}
